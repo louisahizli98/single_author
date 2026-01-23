b7b29428fcf4e8ca5f6c141f0757a5cd21a131d4
louisahizli98 <louisa_hizli@hotmail.com>
2026-01-23 16:27:46 +0100

    January 23, 2026

--- readme.py
+++ readme.py
@@ -7,3 +7,10 @@ print("Hello, Python Project!")
 # conda install numpy pandas
 
 # bc_skewness project is running on conda environment myenv
+
+# To Push New Code to Github Use
+# git status
+# git add .
+# git commit -m "Meaningful message"
+# git push
+
