cb53ebcc68ba99007faa48861ba7ac917db3f5c2
louisahizli98 <louisa_hizli@hotmail.com>
2026-01-23 10:18:44 +0100

    Initial project setup

---
+++ readme.py
@@ -0,0 +1,9 @@
+# January 2026, Louisa Hizli
+
+print("Hello, Python Project!")
+
+# Install Packages the Conda Way (always install packages inside environment)
+# conda activate myenv
+# conda install numpy pandas
+
+# bc_skewness project is running on conda environment myenv
